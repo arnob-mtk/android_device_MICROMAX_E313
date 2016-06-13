3b446c9553159147f1bc4cbf4a5e501489bf5809
arnob-mtk <arnobislam0013@gmail.com>
2016-06-13 11:10:07 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -14,13 +14,13 @@
 
 * Compilation
 
-        # repo init -u git://github.com/fire855/android.git -b cm-12.1
         
-        # repo sync
         
         # source build/envsetup.sh
         
-        # brunch cm_k1_turbo-userdebug
+        # brunch cm_E313-userdebug
+		
+THIS DEVICE TREE IS FOR MICROMAX E313 MT6592SOC POWERED BY ARNOB POROSH
 
 # MTK
 
--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := K1_turbo
+PRODUCT_RELEASE_NAME := E313
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/kingzone/k1_turbo/device_k1-turbo.mk)
+$(call inherit-product, device/MICROMAX/E313/device_E313.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := k1_turbo
-PRODUCT_NAME := cm_k1_turbo
-PRODUCT_BRAND := KINGZONE
-PRODUCT_MODEL := K1_turbo
-PRODUCT_MANUFACTURER := KINGZONE
+PRODUCT_DEVICE := E313
+PRODUCT_NAME := cm_E313
+PRODUCT_BRAND := MICROMAX
+PRODUCT_MODEL := E313
+PRODUCT_MANUFACTURER := E313
